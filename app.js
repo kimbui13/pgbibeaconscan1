e8762c1d63e3f3836ed823cb9d733d394691decd
Kim Bui <kim.bui.perso@gmail.com>
2015-06-13 13:40:42 -0400

    removed test corodva plugins

--- app.js
+++ app.js
@@ -33,12 +33,15 @@ var app = (function()
 
 		// Specify a shortcut for the location manager holding the iBeacon functions.
 		// window.locationManager = cordova.plugins.locationManager;
+
+		/*
 		if( cordova.plugins ) {
 			$('#consolelog').append($('<p>cordova.plugins</p>'));
 		} 
 		if( phonegap.plugins ) {
 			$('#consolelog').append($('<p>phonegap.plugins</p>'));
 		} 
+		*/
 
 		setTimeout( function(){
 			$('#consolelog').append($('<p>Timeout Done</p>'));
